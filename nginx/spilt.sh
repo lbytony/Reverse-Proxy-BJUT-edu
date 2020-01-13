90f2d4ffb7e12ca2c0e01157a853f74e0b21c6c1
lbytony <lbytony@126.com>
2020-01-13 20:59:57 +0800

    For me

--- nginx/spilt.sh
+++ nginx/spilt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 logs_path="/var/log/nginx/"
-logs_path_new="/home/cjw1115/nginx/log/"
+logs_path_new="/home/admin/nginx/log/"
 access_log="access.log"
 error_log="error.log"
 mv "${logs_path}${access_log}" ${logs_path_new}access/access_$(date -d "yesterday" +"%Y%m%d").log
